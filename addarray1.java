cdc49d25e7fe0faa62324e0c4276cc860ab57be8
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-20 16:17:00 +0400

    Update addarray1.java

--- addarray1.java
+++ addarray1.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
-public class addarray1{
-	public static void main(String[] arg){
-		Scanner s=new Scanner(System.in);
-		int [] d=new int [5];
-		for (int i=0; i<d.length; i++){
-			System.out.println("Enter "+ (i+1));
-			d[i]=s.nextInt();
-		}
-		int max=d[0];
-		for(int i=1; i<d.length; i++){
-			if (max<d[i])
-			max=d[i];
-		}
-		System.out.println("maximum number:" + max);
-	}
+public class addarray1 {
+  public static void main(String[] arg) {
+    Scanner s = new Scanner(System.in);
+    int[] d = new int[5];
+    for (int i = 0; i < d.length; i++) {
+      System.out.println("Enter " + (i + 1));
+      d[i] = s.nextInt();
+    }
+    int max = d[0];
+    for (int i = 1; i < d.length; i++) {
+      if (max < d[i])
+        max = d[i];
+    }
+    System.out.println("maximum number:" + max);
+  }
 }
-			
